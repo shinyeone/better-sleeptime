99a875150929762b69f54479366601a9fe3feb14
Esther Yewon Shin <70191776+ye1esther@users.noreply.github.com>
2022-09-01 13:08:17 -0400

    Added colors to hours

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
+const colors = [
+    "white",
+    "red",
+    "white",
+    "red",
+    "white",
+    "red",
+    "white"
+]
+
+
 function handleOnClick() {
     let output = document.querySelector(".output");
     output.style.display = "block";
-  
+
     let hours = document.getElementById("hours");
     hours.innerHTML = "";
-  
+
     let now = new Date();
     // allow 14 minutes to fall sleep!
     now.setMinutes(now.getMinutes() + 14);
-  
+
     // calculate sleep cycles!
     for (let i = 1; i <= 6; i++) {
-      now.setMinutes(now.getMinutes() + 90);
-      hours.innerHTML += now.toLocaleTimeString("en-US", { timeStyle: "short" }) + "<br />";
-    } 
-  }
+        now.setMinutes(now.getMinutes() + 90);
+        let elm = document.createElement("div");
+        elm.innerText =
+            now.toLocaleTimeString("en-US", { timeStyle: "short" });
+        elm.style.color = colors[i];
+        hours.appendChild(elm);
+    }
+}
